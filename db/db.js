85983ba69f5e6e1ad9990db3bf43e1c9aa8cce82
Petar Ostojic <petar.ostojic@pannonia-expertise.net>
2019-06-23 22:05:09 +0200

    Update db.js

--- db/db.js
+++ db/db.js
@@ -2,9 +2,9 @@ var mysql      = require('mysql');
 
 var connection = mysql.createConnection({
     host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'vegan'
+    user     : 'rene_vegan_db',
+    password : 'MpbKPCyfuR2ZCs1U',
+    database : 'rene_vegan_db'
 });
 
 connection.connect();
